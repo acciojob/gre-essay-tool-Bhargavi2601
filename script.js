aa3368fdf289b6346453a9f04ddb2a778c941868
Bhargavi Salivoju <117651365+Bhargavi2601@users.noreply.github.com>
2023-01-23 20:24:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+let str;
+document.getElementById("evaluatedText").value = ""; // initially string is empty
+
+document.addEventListener("keyup", function () {
+	  str = document.getElementById("evaluatedText").value; return string
+	consostr.split(' ').length;
+		 
+ let x = str.split(' ')
+	x= x.filter(item => item);
+	console.log(x)
+	document.querySelector("#wordCount").textContent = x.length;
+	
+    console.log(str);
+	// console.log(str.split(' ').length);
+});
+
